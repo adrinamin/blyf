58f9121ad17aa1b97dadb3e503f0e4277be07cf8
Adrin Amin Salehi <adrin.amin@gmail.com>
2025-01-01 16:18:07 +0000

    Adding boilerplate

--- dev_podman.sh
+++ dev_podman.sh
@@ -3,3 +3,6 @@
 podman build -t blyf:dev .
 
 podman run --rm -d -p 8080:8080 blyf:dev # remove container after execution
+
+# optional cleanup of dangling images
+# podman image prune -f 
--- main.go
+++ main.go
@@ -7,25 +7,40 @@ import (
 )
 
 func main() {
-    fmt.Println("Hello, Podman!")
-
-    // todo: implement upload and download functionality
-    // todo: implement a web server which handles http requests for upload, download and delete
-
-    message := "Welcome to blyf"
-    
-    // definition
-    handler := func (w http.ResponseWriter, req *http.Request) {
-        io.WriteString(w, fmt.Sprintf("%s\n", message))
-    }
-    
     // register
-    http.HandleFunc("/blyf", handler)
+    http.HandleFunc("/blyf", basicMessage)
+    http.HandleFunc("/upload", uploadHandler)
+    http.HandleFunc("/download/", downloadHandler)
+    http.HandleFunc("/delete/", deleteHandler)
 
     // start http server
+    fmt.Println("Starting server on port 8080.")
     err := http.ListenAndServe(":8080", nil)
     if err != nil {
         panic(err)
     }
+}
 
+func basicMessage (w http.ResponseWriter, req *http.Request) {
+    message := "Welcome to blyf"
+   
+    io.WriteString(w, fmt.Sprintf("%s\n", message))
+}
+
+func uploadHandler (w http.ResponseWriter, req *http.Request) {
+    fileUploadMessage := "File upload"
+    fmt.Println("Uploading file...")
+    io.WriteString(w, fmt.Sprintf("%s\n", fileUploadMessage))
+}
+
+func downloadHandler (w http.ResponseWriter, req *http.Request) {
+    fileDownloadMessage := "File download"
+    fmt.Println("Download file...")
+    io.WriteString(w, fmt.Sprintf("%s\n", fileDownloadMessage))
+}
+
+func deleteHandler (w http.ResponseWriter, req *http.Request) {
+    fileDeleteMessage := "File delete"
+    fmt.Println("Delete file...")
+    io.WriteString(w, fmt.Sprintf("%s\n", fileDeleteMessage))
 }
