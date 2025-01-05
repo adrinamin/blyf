e92dfd15e2213e4377ce4400feded92c282dae26
Adrin Amin Salehi <adrin.amin@gmail.com>
2025-01-05 17:31:59 +0000

    Implement upload functionality

--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/fs"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
+
+	fmt.Println("Initializing....")
+	fmt.Println("Creating upload dir")
+	err := os.Mkdir("uploads", fs.ModeDir)
+	if err != nil && !os.IsExist(err) {
+		fmt.Println(err)
+	}
+
 	// register
 	http.HandleFunc("/blyf", basicMessage)
 	http.HandleFunc("/upload", uploadHandler)
@@ -15,7 +27,7 @@ func main() {
 
 	// start http server
 	fmt.Println("Starting server on port 8080.")
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
@@ -39,21 +51,40 @@ func uploadHandler(w http.ResponseWriter, req *http.Request) {
 	// Parse the multipart form, with a maximum memory of 10MB
 	err := req.ParseMultipartForm(10 << 20) // 10MB
 	if err != nil {
+		log.Println(err)
 		http.Error(w, "Error parsing form data", http.StatusBadRequest)
 		return
 	}
 
 	file, handler, err := req.FormFile("file")
 	if err != nil {
+		log.Println(err)
 		http.Error(w, "Error retrieving the file", http.StatusBadRequest)
 		return
 	}
 	defer file.Close()
 
+	// validate filename to avoid directory traversal attacks
+	safeFileName := filepath.Base(handler.Filename)
+	destinationFile, err := os.Create(fmt.Sprintf("uploads/%s", safeFileName))
+	if err != nil {
+		log.Println("Error creating destination", err)
+		http.Error(w, "Error creating destination file", http.StatusInternalServerError)
+		return
+	}
+
+	defer destinationFile.Close()
+
+	if _, err = io.Copy(destinationFile, file); err != nil {
+		log.Println("Error saving destinationFile", err)
+		http.Error(w, "Error saving destinationFile", http.StatusInternalServerError)
+		return
+	}
+
 	// todo: Create a destination file
 	// todo: Copy the uploaded file's content to the destination file
 
-	fmt.Fprintf(w, "Upload of %s was successful.", handler.Filename)
+	fmt.Fprintf(w, "Upload of %s was successful.\n", handler.Filename)
 
 }
 
