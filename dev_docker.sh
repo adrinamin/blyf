c96d7bfc1ec990bc1fb57cbde0c4b852c63e920b
Adrin Amin Salehi <adrin.amin@gmail.com>
2025-01-11 14:11:22 +0000

    Update dev shell scripts

--- dev_docker.sh
+++ dev_docker.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+container_id=$(docker ps -l -q)
+
+if [ -z  "$container_id" ]; then
+    echo "No running containers found."
+    exit 1
+fi
+
+docker stop "$container_id"
+
+# optional cleanup of dangling images
+docker image prune -f
+
 docker build -t blyf:dev .
 
 docker run --rm -d -p 8080:8080 blyf:dev # remove container after execution
--- dev_podman.sh
+++ dev_podman.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+container_id=$(podman ps -l -q)
+
+if [ -z  "$container_id" ]; then
+    echo "No running containers found."
+    exit 1
+fi
+
+podman stop "$container_id"
+
+# optional cleanup of dangling images
+podman image prune -f
+
 podman build -t blyf:dev .
 
 podman run --rm -d -p 8080:8080 blyf:dev # remove container after execution
-
-# optional cleanup of dangling images
-# podman image prune -f 
